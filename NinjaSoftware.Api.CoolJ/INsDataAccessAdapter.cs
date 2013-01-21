7967f879d2f6b089f86938a8c6ffe1b9ce3e1ddf
unknown <marin.masic@HR_MASICM.snt-is.com>
2013-01-21 17:19:01 +0100

    ...

---
+++ NinjaSoftware.Api.CoolJ/INsDataAccessAdapter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NinjaSoftware.CoolJ
+{
+    public interface INsDataAccessAdapter
+    {
+        long UserId { get; set; }
+    }
+}
---
+++ NinjaSoftware.Api.Core/Security.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Security.Cryptography;
+using System.Configuration;
+
+namespace NinjaSoftware.Api.Core
+{
+    public static class Security
+    {
+        public static string GetPasswordHash(string password)
+        {
+            string salt = ConfigurationManager.AppSettings["PasswordSalt"];
+
+            MD5CryptoServiceProvider cryptoProvider = new MD5CryptoServiceProvider();
+
+            byte[] bytes = Encoding.UTF8.GetBytes(string.Format("{0}{1}", salt, password));
+            byte[] hashedBytes = cryptoProvider.ComputeHash(bytes);
+
+            return Convert.ToBase64String(hashedBytes);
+        }
+    }
+}
