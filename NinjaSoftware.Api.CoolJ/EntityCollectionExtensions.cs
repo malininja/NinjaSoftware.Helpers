6ef64e225bad8f0068f4370479255dbe9662503a
unknown <marin.masic@HR_MASICM.snt-is.com>
2013-02-15 11:11:30 +0100

    IEntity2 extesions (update data from other object) EntityCollection extensions (update from json)

---
+++ NinjaSoftware.Api.CoolJ/EntityCollectionExtensions.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SD.LLBLGen.Pro.ORMSupportClasses;
+using Newtonsoft.Json;
+
+namespace NinjaSoftware.Api.CoolJ
+{
+    public static class EntityCollectionExtensions
+    {
+        /// <summary>
+        /// Updates EntityCollection entities with provided jsonData.
+        /// If EntityCollection contains entites which are not included in jsonData, they won't be updated.
+        /// If jsonData contains new entites (primary key is 0 or null), they will be added to EntityCollecton.
+        /// </summary>
+        public static void UpdateEntityCollectionFromJson<TEntity>(this EntityCollectionBase2<TEntity> entityCollection,
+            string jsonData,
+            EntityField2 primaryKeyField,
+            string[] includeFields,
+            string[] excludeFields,
+            JsonSerializerSettings jsonSettings)
+            where TEntity : EntityBase2, IEntity2
+        {
+            IEnumerable<TEntity> deserializedCollection = JsonConvert.DeserializeObject<IEnumerable<TEntity>>(jsonData, jsonSettings);
+
+            if (deserializedCollection.Count() > 0)
+            {
+                foreach (TEntity entity in entityCollection)
+                {
+                    TEntity entityWhichUpdates = deserializedCollection.Where(p => 
+                        object.Equals(p.Fields[primaryKeyField.Name].CurrentValue, entity.Fields[primaryKeyField.Name])).SingleOrDefault();
+
+                    if (null != entityWhichUpdates)
+                    {
+                        entity.UpdateDataFromOtherObject(entityWhichUpdates, includeFields, excludeFields);
+                    }
+                }
+
+                foreach (TEntity entity in deserializedCollection.Where(e =>
+                    object.Equals(e.Fields[primaryKeyField.Name].CurrentValue, 0L) || e.Fields[primaryKeyField.Name].CurrentValue == null))
+                {
+                    entityCollection.Add(entity);
+                }
+            }
+        }
+
+        public static List<TEntity> GetEntitiesNotIncludedInJson<TEntity>(this EntityCollectionBase2<TEntity> entityCollection, string jsonData)
+            where TEntity : EntityBase2, IEntity2
+        {
+            List<TEntity> toReturn = new List<TEntity>();
+
+            if (entityCollection.Count > 0)
+            {
+                IEnumerable<TEntity> deserializedCollection = JsonConvert.DeserializeObject<IEnumerable<TEntity>>(jsonData);
+                string pkName = entityCollection[0].GetPrimaryKeyName();
+
+                foreach (TEntity entity in entityCollection)
+                {
+                    TEntity entityWhichUpdates = deserializedCollection.Where(e => 
+                        object.Equals(e.Fields[pkName].CurrentValue, entity.Fields[pkName])).SingleOrDefault();
+
+                    if (null == entityWhichUpdates)
+                    {
+                        toReturn.Add(entity);
+                    }
+                }
+            }
+
+            return toReturn;
+        }
+    }
+}
---
+++ NinjaSoftware.Api.CoolJ/IEntity2Extensions.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SD.LLBLGen.Pro.ORMSupportClasses;
+
+namespace NinjaSoftware.Api.CoolJ
+{
+    public static class IEntity2Extensions
+    {
+        /// <summary>
+        /// Sets Field.CurrentValue from entityWhichUpdates to entityToUpdate.
+        /// Doesn't change primary key field.
+        /// </summary>
+        public static void UpdateDataFromOtherObject(this IEntity2 entityToUpdate,
+            IEntity2 entityWhichUpdates,
+            string[] includeFields,
+            string[] excludeFields)
+        {
+            foreach (IEntityField2 field in entityToUpdate.Fields)
+            {
+                if ((null == includeFields || includeFields.Contains(field.Name)) &&
+                    (null == includeFields || !excludeFields.Contains(field.Name)))
+                {
+                    if (!field.IsPrimaryKey &&
+                        !object.Equals(field.CurrentValue, entityWhichUpdates.Fields[field.Name].CurrentValue))
+                    {
+                        field.CurrentValue = entityWhichUpdates.Fields[field.Name].CurrentValue;
+                        field.IsChanged = true;
+                        entityToUpdate.IsDirty = true;
+                    }
+                }
+            }
+        }
+
+        public static string GetPrimaryKeyName(this IEntity2 entity)
+        {
+            string toReturn = null;
+
+            foreach (IEntityField2 field in entity.Fields)
+            {
+                if (field.IsPrimaryKey)
+                {
+                    toReturn = field.Name;
+                    break;
+                }
+            }
+
+            if (string.IsNullOrWhiteSpace(toReturn))
+            {
+                throw new ORMException("Entity must have a primary key.");
+            }
+
+            return toReturn;
+        }
+    }
+}
